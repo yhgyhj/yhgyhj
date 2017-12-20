0fd48ab8c7df54ef5cd65133a6364e324c612daf
yhgyhj <2314813879@qq.com>
2017-12-20 10:56:16 +0800

    没有

---
+++ demo/src/com/yhj/Demo/Demo04.java
@@ -0,0 +1,12 @@
+package com.yhj.Demo;
+public class Demo04{
+	
+	public static void main(String[] args){
+    //三目运算符
+	  String s=2<3?"表达式为真":"表达式为假";
+	  System.out.println("s="+s);
+	  boolean a=2<3?true:false;
+	  System.out.println("a="+a);
+	}
+	}
+			
